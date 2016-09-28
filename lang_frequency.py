945e1f7b4a94d3a633f93196e12355b23a52537f
Talkytitan5127 <pachagorbunoff@yandex.ru>
2016-09-28 18:05:13 +0300

    Add files via upload

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,21 @@
+fi=open("Текст.txt",'r');
+dict_slov={}
+s=fi.read().split()
+print(s)
+for sl in s:
+    if not(sl[-1].isalpha()):
+        sl=sl[:-1]
+    sl=sl.lower()
+    if sl not in dict_slov:
+        dict_slov[sl]=-1
+    else:
+        dict_slov[sl]-=1
+vivod=[]
+for sl in dict_slov.keys():
+    vivod.append((-dict_slov[sl],sl))
+index=1
+for elem in sorted(vivod):
+    if index==11: break
+    print(index,')',' ',elem[1],sep='')
+    index+=1
 
-
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
-
-
-if __name__ == '__main__':
-    pass
