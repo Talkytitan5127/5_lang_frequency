142df3156a647cf102bb5d13c17d81eaacfcd1b8
Talkytitan5127 <pachagorbunoff@yandex.ru>
2016-10-19 16:15:12 +0300

    Add files via upload

--- 5_lang_frequency.py
+++ 5_lang_frequency.py
@@ -2,23 +2,28 @@ import os
 import re
 from collections import Counter
 
-
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    return re.findall(r'\w+', open(filepath).read().lower())
+    with open(filepath,"r") as file_handler:
+        return file_handler.read().lower()
 
 def count_words(data):
-    return Counter(file).most_common(10)
+    quantity_of_encounter_words=10
+    words=re.findall(r'\w+',data)
+    return Counter(words).most_common(quantity_of_encounter_words)
 
 def print_words(data):
-    for word in data:
-        print(*word)
+    print("The most frequent words in this text is:")
+    for word_number,word in enumerate(data):
+        print(word_number+1,')',sep='',end=' ')
+        print('"',word[0],'"',sep='',end=' ')
+        print("Amount =",word[1])
 
-    
 if __name__=='__main__':
     print("Enter the path to the file")
     file=load_data(input())
     print_words(count_words(file))
+    
       
     
