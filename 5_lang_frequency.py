2676d9d40052b5aa573ec78eff3afa87431c69eb
Talkytitan5127 <pachagorbunoff@yandex.ru>
2016-10-25 20:34:43 +0300

    Update 5_lang_frequency.py

--- 5_lang_frequency.py
+++ 5_lang_frequency.py
@@ -2,28 +2,26 @@ import os
 import re
 from collections import Counter
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open(filepath,"r") as file_handler:
+    with open(filepath, "r") as file_handler:
         return file_handler.read().lower()
 
+
 def count_words(data):
-    quantity_of_encounter_words=10
-    words=re.findall(r'\w+',data)
+    quantity_of_encounter_words = 10
+    words = re.findall(r"\w+", data)
     return Counter(words).most_common(quantity_of_encounter_words)
+
 
 def print_words(data):
     print("The most frequent words in this text is:")
-    for word_number,word in enumerate(data):
-        print(word_number+1,')',sep='',end=' ')
-        print('"',word[0],'"',sep='',end=' ')
-        print("Amount =",word[1])
+    for word_number, word in enumerate(data):
+        print("{}) \"{}\" Amount = {}".format(word_number+1, word[0], word[1]))
 
-if __name__=='__main__':
-    print("Enter the path to the file")
-    file=load_data(input())
+if __name__ == "__main__":
+    file = load_data(input("Enter the path to the file:"))
     print_words(count_words(file))
-    
-      
     
